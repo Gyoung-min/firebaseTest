12c1ced89d63db27a73eaacf7826a6675e8129cd
Min <rudals0326@gmail.com>
2022-08-28 14:57:25 +0900

    initialize project

---
+++ src/test/java/test/firebasetest/FirebaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.firebasetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
